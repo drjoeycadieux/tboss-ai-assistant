9cdf5306085becb4a20992a8a73d4d94b68197e1
joeycadieux161 <joeycadieux161@gmail.com>
2024-11-23 17:15:28 +0000

    inc. AI Roll out

---
+++ main-text.js
@@ -0,0 +1,58 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import MarkdownIt from 'markdown-it';
+import { maybeShowApiKeyBanner } from './gemini-api-banner';
+import './style.css';
+
+// 🔥🔥 FILL THIS OUT FIRST! 🔥🔥
+// Get your Gemini API key by:
+// - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
+// - Or by visiting https://g.co/ai/idxGetGeminiKey
+let API_KEY = 'AIzaSyA_-l4-8Otmcq8EKmOJ5LaVroGlb5GeGRc';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Generating...';
+
+  try {
+    // Assemble the prompt with just the text input (no image)
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { text: promptInput.value }
+        ]
+      }
+    ];
+
+    // Call the multimodal model, and get a stream of results
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-pro", // or gemini-1.5-flash
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+      ],
+    });
+
+    const result = await model.generateContentStream({ contents });
+
+    // Read from the stream and interpret the output as markdown
+    let buffer = [];
+    let md = new MarkdownIt();
+    for await (let response of result.stream) {
+      buffer.push(response.text());
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
+
+// You can delete this once you've filled out an API key
+maybeShowApiKeyBanner(API_KEY);
